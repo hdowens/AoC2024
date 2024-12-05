4eb893e32f721f5871b5f7abd6460b69c51e4d80
Harry Owens <harryowens23@gmail.com>
2024-12-05 14:17:44 -0500

    cleaned up the code to remove all unused code. There probs is a faster way of doing this with sets and merging part1 and 2 into the same loop, I just dont have the time to do that

--- day5/day5.py
+++ day5/day5.py
@@ -3,45 +3,33 @@ from pathlib import Path
 
 def part1(rules, instructions) -> None:
     total = 0
-    for idx, it in enumerate(instructions):        
+    for it in instructions:
         err_flag = True
         for i in range(0, len(it)):
-            #print(f"Testing element [{it[i]}]")
-            for j in range(i+1, len(it)):
-                #print(f"\tF: [{it[j]}|{it[i]}] must not exist")
+            for j in range(i + 1, len(it)):
                 if (it[j], it[i]) in rules and err_flag:
-                    #print(f"\tF [{idx}] failed because {(it[j], it[i])} exists")
                     err_flag = False
-            #for k in range(i, -1, -1):
-            #    if it[i] != it[k]:
-            #        if (it[i], it[k]) in rules and err_flag:
-            #        #print(f"\tB: [{it[i]}|{it[k]}] must not exist")
-            #            print(f"\tB [{idx}] failed because {(it[i], it[k])} exists")
-            #            err_flag = False
 
         if err_flag:
-            #print(f"\t[{idx}] {it} is correct")
-            total += it[(len(it)-1) // 2]
+            total += it[(len(it) - 1) // 2]
 
     return total
 
+
 def part2(rules, instructions) -> None:
     new_insrts = []
-    for idx, it in enumerate(instructions):
-        err_flag = True        
+    for it in instructions:
+        err_flag = True
         for i in range(0, len(it)):
-            for j in range(i+1, len(it)):
+            for j in range(i + 1, len(it)):
                 if (it[j], it[i]) in rules:
                     err_flag = False
-                    #print(f"F [{idx}] failed because {(it[j], it[i])} exists")
                     it[j], it[i] = it[i], it[j]
-        
+
         if not err_flag:
             new_insrts.append(it)
 
-
-    return sum(it[(len(it)-1) // 2] for it in new_insrts)
-
+    return sum(it[(len(it) - 1) // 2] for it in new_insrts)
 
 
 def main() -> None:
@@ -49,11 +37,11 @@ def main() -> None:
     with open(p.resolve(), "r") as input:
         data = input.read().split("\n\n")
 
-    #lets transform the rules into a list of tuples
+    # lets transform the rules into a list of tuples
     rules = [tuple(map(int, s.split("|"))) for s in data[0].split("\n")]
 
-    #lets transform the instructions into a list of list of integers    
-    instructions = [list(map(int, sample.split(','))) for sample in data[1].split("\n")]
+    # lets transform the instructions into a list of list of integers
+    instructions = [list(map(int, sample.split(","))) for sample in data[1].split("\n")]
 
     print(f"Part 1: {part1(rules, instructions)}")
     print(f"Part 2: {part2(rules, instructions)}")
@@ -61,5 +49,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
-
