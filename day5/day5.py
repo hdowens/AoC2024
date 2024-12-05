608a5ce361722f3ab821f399eb905165d8e2836a
Harry Owens <harryowens23@gmail.com>
2024-12-05 13:46:08 -0500

    Day 5 was slightly easier than day 4 IMO, despite the problem description being very convoluted. Roll on day 6

---
+++ day5/day5.py
@@ -0,0 +1,63 @@
+from pathlib import Path
+
+
+def part1(rules, instructions) -> None:
+    total = 0
+    for idx, it in enumerate(instructions):        
+        err_flag = True
+        for i in range(0, len(it)):
+            #print(f"Testing element [{it[i]}]")
+            for j in range(i+1, len(it)):
+                #print(f"\tF: [{it[j]}|{it[i]}] must not exist")
+                if (it[j], it[i]) in rules and err_flag:
+                    print(f"\tF [{idx}] failed because {(it[j], it[i])} exists")
+                    err_flag = False
+            #for k in range(i, -1, -1):
+            #    if it[i] != it[k]:
+            #        if (it[i], it[k]) in rules and err_flag:
+            #        #print(f"\tB: [{it[i]}|{it[k]}] must not exist")
+            #            print(f"\tB [{idx}] failed because {(it[i], it[k])} exists")
+            #            err_flag = False
+
+        if err_flag:
+            print(f"\t[{idx}] {it} is correct")
+            total += it[(len(it)-1) // 2]
+
+
+    print(total)
+
+
+def main() -> None:
+    p = Path("puz_input.txt")
+    with open(p.resolve(), "r") as input:
+        data = input.read().split("\n\n")
+
+    #lets transform the rules into a list of tuples
+    rules = [tuple(map(int, s.split("|"))) for s in data[0].split("\n")]
+
+    #lets transform the instructions into a list of list of integers    
+    instructions = [list(map(int, sample.split(','))) for sample in data[1].split("\n")]
+
+    #part1(rules, instructions)
+    new_insrts = []
+    for idx, it in enumerate(instructions):
+        err_flag = True        
+        for i in range(0, len(it)):
+            #print(f"Testing element [{it[i]}]")
+            for j in range(i+1, len(it)):
+                #print(f"\tF: [{it[j]}|{it[i]}] must not exist")
+                if (it[j], it[i]) in rules:
+                    err_flag = False
+                    print(f"F [{idx}] failed because {(it[j], it[i])} exists")
+                    it[j], it[i] = it[i], it[j]
+        
+        if not err_flag:
+            new_insrts.append(it)
+
+
+    print(sum(it[(len(it)-1) // 2] for it in new_insrts))
+
+if __name__ == "__main__":
+    main()
+
+
